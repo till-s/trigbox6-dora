53b7c3a799e41b4da2683c7804c3e040f9d8a105
till straumann <till.straumann@alumni.tu-berlin.de>
2019-05-24 17:02:54 -0700

    gitified

---
+++ DoraApp.py
@@ -0,0 +1,102 @@
+#!/usr/local/bin/python3
+import pycpsw
+import pathGrep
+import time
+import IrqHandler
+import ClockSwitcher
+import re
+import os
+import YamlFixup
+from   flask import render_template
+
+deviceTopName = "Timing-Trigger Box 7"
+
+def TboxSimplePage():
+  return render_template(simpleTemplateName, deviceTopName = deviceTopName)
+
+class YamlFixup(YamlFixup.YamlFixup):
+  def __init__(self, optDict, argList):
+    super(YamlFixup, self).__init__(optDict, argList)
+
+  def findNode(self, rn, path):
+    n = pycpsw.YamlFixup.findByName(rn, path)
+    if not n.IsDefined() or n.IsNull():
+      raise RuntimeError("tbox.YamlFixup: YAML node '" + path + "' not found")
+    return n
+ 
+
+  def __call__(self, rootNode, topNode):
+    super(YamlFixup, self).__call__(rootNode, topNode)
+    p = "/mmio/AmcCarrierCore/AmcCarrierTiming/EvrV2CoreTriggers/"
+    trgs  = self.findNode(rootNode, p.replace("/","/children/"))
+    chreg = self.findNode(trgs,     "EvrV2ChannelReg/children")
+    n     = self.findNode(chreg,   "BsaEnabled")
+    n["instantiate"] = "False"
+    n     = self.findNode(chreg,   "BsaWindowDelay")
+    n["instantiate"] = "False"
+    n     = self.findNode(chreg,   "BsaWindowWidth")
+    n["instantiate"] = "False"
+    n     = self.findNode(chreg,   "BsaWindowSetup")
+    n["instantiate"] = "False"
+    n     = self.findNode(chreg,   "DmaEnabled")
+    n["instantiate"] = "False"
+    chreg = self.findNode(trgs,     "EvrV2ChannelReg")
+    n     = self.findNode(chreg,   "at")
+    n["nelms"] = "7"
+    chreg = self.findNode(trgs,     "EvrV2TriggerReg")
+    n     = self.findNode(chreg,   "at")
+    n["nelms"] = "7"
+
+class DoraApp(object):
+  def __init__(self):
+    super(DoraApp, self).__init__()
+
+  def getYamlFixup(self, optDict, mainArgs):
+    self.optDict_ = optDict
+    return YamlFixup( optDict, mainArgs )
+
+  def getTopLevelName(self):
+    return deviceTopName
+
+  def initApp(self, cpswRoot, flaskApp):
+    self.flaskApp_ = flaskApp
+    self.flaskApp_.add_url_rule('/simple', 'simple', TboxSimplePage)
+    print("URL RULE ADDED")
+    if self.optDict_["UseNullDev"]:
+      return
+    self.confDir_  = "../yaml/"
+    self.cpswRoot_ = cpswRoot
+    r              = self.cpswRoot_
+    pg             = pathGrep.PathGrep( r, asPath=True)
+    sysincal       = pycpsw.ScalVal_RO.create(pg("/Si5344/SYSINCAL$")[0])
+    if 0 != sysincal.getVal():
+      r.loadConfigFromYamlFile( self.confDir_ + "defaults_si5344.yaml" )
+    for i in range(10):
+      if 0 == sysincal.getVal():
+        break
+      time.sleep(0.5)
+    else:
+      raise RuntimeError("SI5344 would not finish calibrating")
+    r.loadConfigFromYamlFile( self.confDir_ + "defaults.yaml" )
+    self.irqHdlr_  = IrqHandler.IrqHandler( r, [ClockSwitcher.ClockSwitchAction(r, self.confDir_ )])
+    self.irqHdlr_.start()
+
+  def getDebugProbesPath(self):
+    if self.optDict_["UseNullDev"]:
+      return None
+    rp = os.path.realpath("/lib/firmware/zynq-firmware.bin")
+    ltxp = re.sub("[.]bin([.]swab)?", ".ltx", rp)
+    if os.path.isfile( ltxp ):
+      return ltxp
+    else:
+      return None
+
+  def genHtml(self, appDb, chksumAsString, yamlHasChanged):
+    global simpleTemplateName
+    simpleTemplateName = "guts-simple-"+chksumAsString+".html"
+    tmplPath           = "templates/" + simpleTemplateName
+    if yamlHasChanged or not os.path.isfile( tmplPath ):
+      genSimple.GenSimple( appDb, tmplPath )
+
+  def getAppLinks(self):
+    return [ { "ref": "/simple", "txt": "Trigger Controls" } ]
---
+++ genSimple.py
@@ -0,0 +1,115 @@
+import re
+import pathGrep as pg
+import sys
+import jinja2
+import io
+
+j2env = jinja2.Environment(
+         loader     = jinja2.PackageLoader('genSimple','templates'),
+         autoescape = jinja2.select_autoescape(['html', 'xml'])
+       )
+
+class GenSimple(object):
+  def __init__(self, theDb, tmplName):
+    self._level = 0
+    self._db    = theDb
+    self._fd    = io.open(tmplName, "w")
+    print( '{% extends "simple.html" %}', file = self._fd )
+    print( '{% block content         %}', file = self._fd )
+    print( '<table id="miscTable">',      file = self._fd )
+    print( '<caption>General Configuration</caption>', file = self._fd )
+    print( '<tr> <td> <a href="/"     class="realLink">General Info</a> </td> </tr>', file = self._fd )
+    print( '<tr> <td> <a href="/tree" class="realLink">Expert Panel</a> </td> </tr>', file = self._fd )
+    print( '<tr> <td> <form method="post" action="/saveConfig?file=config/simple.yaml">', file = self._fd )
+    print( '     <button class="configButton" type="submit">Save Configuration</label>', file = self._fd )
+    print( '     </form> </td>',          file = self._fd )
+    print( '<tr> <td>',                   file = self._fd )
+    print( '	<label class="configButton">Load Configuration<input type="file" class="loadConfig"> </label>', file = self._fd )
+    print( '</td> </tr>',                 file = self._fd )
+    print( '<tr>',                        file = self._fd )
+    self.genTab(".*TimingFrameRx/{}", [ ["ClkSel", ""], ["FidCount", ""] ])
+    print( '</tr>',                       file = self._fd )
+    print( '</table>',                    file = self._fd )
+    nams        = []
+    nams.append( ["Width",      ''] )
+    nams.append( ["Delay",      ''] )
+    nams.append( ["Polarity",   ''] )
+    nams.append( ["Enable",     ''] )
+    nams.append( ["Source",     ' class="selSourceMode"'] )
+    print( '<table id="outputTable" class="outputTable">',    file = self._fd )
+    print( '<caption>Trigger Output Configuration</caption>', file = self._fd )
+    print( '<tr>', file = self._fd )
+    print( '<th>Output</th>', file = self._fd )
+    for i in range( len( theDb.pg( ".*EvrV2TriggerReg[^/]*$" ) ) - 1, -1 , -1 ):
+      print('<th>{}</th>'.format(i),      file = self._fd)
+    print( '</tr>',                       file = self._fd )
+    self.genTab( ".*EvrV2TriggerReg.*/{}$", nams )
+    print( '</table>',                    file = self._fd )
+    #
+    nChs = len( theDb.pg( ".*EvrV2ChannelReg[^/]*$" ) )
+    itms = []
+    itms.append( ["Enable",         ''] )
+    itms.append( ["Count",          ''] )
+    itms.append( ["DestSelMode",    ''] )
+    itms.append( ["DestSelMask",    ''] )
+    itms.append( ["RateSelMode",    ' class="selRateMode"'] )
+    itms.append( ["ACRate",         ' class="rateModeAC isModal"']    )
+    itms.append( ["ACTimeSlotMask", ' class="rateModeAC isModal"']    )
+    itms.append( ["FixedRate",      ' class="rateModeFixed isModal"'] )
+    itms.append( ["SequenceSelect", ' class="rateModeSeq isModal"']   )
+    print( '<table id="allChannels">',      file = self._fd )
+    print( '<caption>Channel Configuration</caption>', file = self._fd )
+    print( '<tr    id="allChannelsRow">',   file = self._fd )
+    for i in range(nChs):
+      print( '<tr><td><p class="caret colmn{} chnllab">Channel {}'.format(i,i), file = self._fd )
+      print( '<table id="channelTable{}" class="channelTable nested">'.format(i),   file = self._fd )
+      for itm in itms:
+        self.genTab( ".*EvrV2ChannelReg[\[]{}]/{}$".format(i,'{}'), [itm], colOff = i, withLab = True)
+      print( '</table></p></td></tr',              file = self._fd )
+    print( '</tr>',                        file = self._fd )
+    print( '</table>',                     file = self._fd )
+    print( '{% endblock content      %}', file = self._fd )
+    self._fd.close()
+
+  def genTab(self, patt, itms, colOff = 0, withLab = None):
+    for itm in itms:
+      nam         = itm[0]
+      paths       = self._db.pg( patt.format(nam) )
+      i           = 0
+      print( "<tr{}>".format(itm[1]), file = self._fd )
+      for p in paths:
+        try:
+          el = self._db.lkups( p.toString() )
+        except KeyError as e:
+          print("Failure to find key for ", p.toString())
+          raise
+        tag, clss, atts, xtra, xcol = el.getHtml( self._level )
+        if 0 == i:
+          #print( '<td display="None"><div class="tooltip"><p>', file = self._fd )
+          #print( '{%- autoescape true -%}',  file = self._fd )
+          with el as theel:
+            tip = theel[1].getDescription()
+          #print( '{%- endautoescape   -%}',  file = self._fd )
+          #print( '</p></div></td>',               file = self._fd )
+          if None != withLab and not withLab:
+            nam = None
+        else:
+          if None == withLab or not withLab:
+            nam = None
+        desc = None
+        elem = j2env.get_template('elem.html')
+        clss += " toolTipper colmn{}".format(colOff + i)
+        print( elem.render(
+                            name    = nam,
+                            tag     = tag,
+                            id      = el.getHtmlId(),
+                            classes = clss,
+                            atts    = atts,
+                            xtras   = xtra,
+                            xcol    = None,
+                            desc    = desc,
+                            tooltip = tip,
+                            level   = self._level),
+                            file = self._fd )
+        i += 1
+      print( "</tr>", file = self._fd )
